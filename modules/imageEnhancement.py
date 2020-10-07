c024d181a0668f59a112497b6ed34502a069fd94
Shrey Shah <shreyshah00@gmail.com>
2020-10-07 23:52:25 +0530

    Added image enhancement techniques

---
+++ modules/imageEnhancement.py
@@ -0,0 +1,31 @@
+#Importing necessary libraries
+import cv2
+import numpy as np
+from numpy.fft import fft2, ifft2
+from scipy.signal import gaussian, convolve2d
+
+
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+        for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+def wiener_filter(img, kernel, K):
+    kernel /= np.sum(kernel)
+    dummy = np.copy(img)
+    dummy = fft2(dummy)
+    kernel = fft2(kernel, s = img.shape)
+    kernel = np.conj(kernel) / (np.abs(kernel) ** 2 + K)
+    dummy = dummy * kernel
+    dummy = np.abs(ifft2(dummy))
+    return dummy
+
+def gaussian_kernel(kernel_size = 3):
+    h = gaussian(kernel_size, kernel_size / 3).reshape(kernel_size, 1)
+    h = np.dot(h, h.transpose())
+    h /= np.sum(h)
+    return h
